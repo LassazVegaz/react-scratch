b5934e6f6286004f06458fdc496d31072cf39357
LassazVegaz <lasindunuwanga@gmail.com>
2022-08-03 14:25:29 +0530

    init proj

---
+++ App.jsx
@@ -0,0 +1,12 @@
+const Heading = () => {
+	return <h1 class="heading">Hello World!</h1>;
+};
+
+export const App = () => {
+	return (
+		<div>
+			<Heading />
+			<span>Coming from react built from scratch</span>
+		</div>
+	);
+};
